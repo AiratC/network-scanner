64232bc67d2559a549aacc9b16ef579e392f0612
AiratC <117903491+AiratC@users.noreply.github.com>
2026-07-09 00:59:58 +0300

    Update database.py
    delete url db

--- backend/utils/database.py
+++ backend/utils/database.py
@@ -1,13 +1,13 @@
 import asyncpg
 
-DATABASE_URL = "postgresql://postgres:airat@localhost:5432/network_scanner"
+
 
 # Глобальная переменная где будет храниться пул соединений
 pool = None
 
 async def init_db():
    global pool
-   pool = await asyncpg.create_pool(DATABASE_URL)
+   pool = await asyncpg.create_pool(process.env.DATABASE_URL)
    
 async def close_db():
    global pool
